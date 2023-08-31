b5127d6de20e382cb8c79653425430cd9efce1ab
jkulds <w84my5@yandex.ru>
2023-09-01 02:03:01 +0300

    added some JwtOptions fields

--- Jwt/JwtOptions.cs
+++ Jwt/JwtOptions.cs
@@ -1,35 +1,96 @@
 ﻿using System.Text;
 using Jkulds.Micro.Options.Base;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
 namespace Jkulds.Micro.Options.Jwt;
 
+/// <summary>
+/// Jwt options for appsettings.json
+/// for process add "Jwt" key to appsettings.json with fields described after
+/// </summary>
 public class JwtOptions : BaseOption
 {
     public string Key { get; set; } = string.Empty;
+    public bool? RequireExpirationTime { get; set; }
+    public bool? ValidateIssuerSigningKey { get; set; } 
+    public string? Issuer { get; set; }
+    public bool? ValidateIssuer { get; set; }
+    public string? Audience { get; set; }
+    public bool? ValidateAudience { get; set; }
+    public bool? ValidateLifetime { get; set; }
+    public string Algorithm { get; set; } = string.Empty;
+    private byte[] KeyBytes => Encoding.ASCII.GetBytes(Key);
+    
     public TimeSpan TokenExpire { get; set; } = TimeSpan.Zero;
     public TimeSpan RefreshTokenExpire { get; set; } = TimeSpan.Zero;
-    public string Issuer { get; set; } = string.Empty;
-    public bool ValidateIssuer { get; set; } = false;
-    public string Audience { get; set; } = string.Empty;
-    public bool ValidateAudience { get; set; }
-    public string Authority { get; set; } = string.Empty;
-    public bool RequireHttpsMetadata { get; set; } = false;
-    public string Algorithm { get; set; } = string.Empty;
-    public byte[] KeyBytes => Encoding.ASCII.GetBytes(Key);
 
+    /// <summary>
+    /// option for JwtBearerOptions <see cref="JwtBearerOptions.RequireHttpsMetadata"/>
+    /// </summary>
+    public bool? RequireHttpsMetadata { get; set; }
+    
+    public string? Authority { get; set; }
+
+    /// <summary>
+    /// option for JwtBearerOptions <see cref="JwtBearerOptions.Authority"/>
+    /// </summary>
     public TokenValidationParameters GetTokenValidationParameters()
     {
-        return new TokenValidationParameters
+        var result = new TokenValidationParameters();
+        result.IssuerSigningKey = new SymmetricSecurityKey(KeyBytes);
+
+        if (ValidateIssuerSigningKey.HasValue)
         {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(KeyBytes),
-            ValidateIssuer = ValidateIssuer,
-            ValidIssuer = Issuer,
-            ValidateAudience = ValidateAudience,
-            RequireExpirationTime = false,
-            ValidateLifetime = false,
-            ValidAudience = Audience,
-        };
+            result.ValidateIssuerSigningKey = ValidateIssuerSigningKey.Value;
+        }
+
+        if (ValidateIssuer.HasValue)
+        {
+            result.ValidateIssuer = ValidateIssuer.Value;
+        }
+        
+        if (ValidateAudience.HasValue)
+        {
+            result.ValidateIssuer = ValidateAudience.Value;
+        }
+
+        if (!string.IsNullOrEmpty(Issuer))
+        {
+            result.ValidIssuer = Issuer;
+        }
+        
+        if (!string.IsNullOrEmpty(Audience))
+        {
+            result.ValidAudience = Audience;
+        }
+
+        if (RequireExpirationTime.HasValue)
+        {
+            result.RequireExpirationTime = RequireExpirationTime.Value;
+        }
+
+        if (ValidateLifetime.HasValue)
+        {
+            result.ValidateLifetime = ValidateLifetime.Value;
+        }
+
+        return result;;
+    }
+
+    public void AddAuthorityToOptions(JwtBearerOptions jwtOptions)
+    {
+        if (!string.IsNullOrEmpty(Authority))
+        {
+            jwtOptions.Authority = Authority;
+        }
+    }
+    
+    public void AddRequireHttpsMetadataToOptions(JwtBearerOptions jwtOptions)
+    {
+        if (RequireHttpsMetadata.HasValue)
+        {
+            jwtOptions.RequireHttpsMetadata = RequireHttpsMetadata.Value;
+        }
     }
 }
